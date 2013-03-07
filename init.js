44957c0c2322b1afe2e4dd5003a2364b79a2dda8
Franz Heidl <franz@franzheidl.de>
2013-03-07 02:19:13 +0100

    fix conditional

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ Hooks.addMenuItem("Edit/Copy File Path", "ctrl-cmd-p", function() {
         Alert.show("Oops!", "There must be an open file to copy the path from.", ["OK, got it."]);
         return;
     }
-    if (!Document.current().isUntitled()) {
+    if (!Document.current().path()) {
         Alert.show("Oops!", "There is no path to copy yet. Save your document first.", ["OK, promised."]);
         return;
     }
