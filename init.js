f9413d00a7ccf845df765d094d466126a4b3de93
Franz Heidl <franz@franzheidl.de>
2013-03-07 01:35:33 +0100

    Update alert if unsaved document

--- init.js
+++ init.js
@@ -1,6 +1,10 @@
 Hooks.addMenuItem("Edit/Copy File Path", "ctrl-cmd-p", function() {
-    if (!Document.current() || !Document.current().path()) {
-        Alert.show("Oops!", "There must be an open file to copy the path from.", ["Got it."]);
+    if (!Document.current()) {
+        Alert.show("Oops!", "There must be an open file to copy the path from.", ["OK, got it."]);
+        return;
+    }
+    if (!Document.current().isUntitled()) {
+        Alert.show("Oops!", "There is no path to copy yet. Save your document first.", ["OK, promised."]);
         return;
     }
     var currentFilePath = (Document.current()).path();
